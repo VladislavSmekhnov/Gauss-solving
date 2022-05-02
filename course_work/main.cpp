49c12a217469364aed476ec3e3ded10c16605f7d
Vlad_Sm <vladislav.smekhnov@gmail.com>
2022-05-02 18:06:48 +0300

    Added solution check

--- course_work/main.cpp
+++ course_work/main.cpp
@@ -4,6 +4,10 @@
 #include "gauss_serial.h"
 #include "gauss_parallel.h"
 
+bool check_answer(std::vector<double> answer, std::vector<double> freeMatrixCloumn, std::vector<std::vector<double>> matrix);
+void create_new_freeColumn(std::vector<double>& v, std::vector<double>& answer, std::vector<std::vector<double>> matrix);
+void keep_4_digits_after_point(std::vector<double>& v);
+
 int main()
 {
     setlocale(LC_ALL, "utf-8");
@@ -11,8 +15,8 @@ int main()
     int m; // количество строк
     int n; // количество столбцов
     double value; // значение элемента массива
-    std::vector<std::vector<double>> matrix_s, matrix_p; // матрица элементов без столбца свободных членов
-    std::vector<double> freeMatrixColumn_s, freeMatrixColumn_p; // столбец свободных членов
+    std::vector<std::vector<double>> matrix_s, matrix_p, matrixForChecking; // матрица элементов без столбца свободных членов
+    std::vector<double> freeMatrixColumn_s, freeMatrixColumn_p, freeMatrixColumnForChecking; // столбец свободных членов
 
     printf("Решение СЛАУ методом Гаусса\n");
     // Задаём размерность матрицы
@@ -21,6 +25,7 @@ int main()
     std::cin >> m;
     matrix_s.resize(m);
     matrix_p.resize(m);
+    matrixForChecking.resize(m);
     printf("Количество столбцов: ");
     std::cin >> n;
     // Заполняем матрицу
@@ -32,6 +37,7 @@ int main()
             std::cin >> value;
             matrix_s[i].push_back(value);
             matrix_p[i].push_back(value);
+            matrixForChecking[i].push_back(value);
         }
     }
     printf("Теперь заполните столбец свободных членов:\n");
@@ -40,10 +46,11 @@ int main()
         std::cin >> value;
         freeMatrixColumn_s.push_back(value);
         freeMatrixColumn_p.push_back(value);
+        freeMatrixColumnForChecking.push_back(value);
     }
 
     /* Последовательное решение : */
-    printf("Решаю...\n\n");
+    printf("\nРешаю...\n\n");
     printf("Ответ (последовательное решение):\n");
     std::vector<double> answer1 = gauss_solving(matrix_s, freeMatrixColumn_s, m);
     // Ответ:
@@ -61,7 +68,53 @@ int main()
 
     printf("\n");
     /* Конец параллельного решения */
+    printf("\nПроверка решения\n\n");
+    /* Проверка последовательного решения */
+    std::cout << std::boolalpha << "Последовательное решение верно:\t" <<
+        check_answer(answer1, freeMatrixColumnForChecking, matrixForChecking) << std::endl;
+
+    /* Проверка параллельного решения */
+    std::cout << std::boolalpha << "Параллельное решение верно:\t" <<
+        check_answer(answer2, freeMatrixColumnForChecking, matrixForChecking) << std::endl;
 
     system("pause");
     return 0;
+}
+
+bool check_answer(std::vector<double> answer, std::vector<double> freeMatrixCloumn, std::vector<std::vector<double>> matrix)
+{
+    std::vector<double> myFreeColumn(freeMatrixCloumn.size(), 0.0);
+
+    create_new_freeColumn(myFreeColumn, answer, matrix);
+    
+    for (int i = 0; i < freeMatrixCloumn.size(); i++)
+    {
+        if (myFreeColumn[i] != freeMatrixCloumn[i])
+            return false;
+    }
+
+    return true;
+}
+
+void create_new_freeColumn(std::vector<double> &v, std::vector<double>& answer, std::vector<std::vector<double>> matrix)
+{
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix.size(); j++)
+            v[i] += matrix[i][j] * answer[j];
+    }
+    keep_4_digits_after_point(v);
+}
+
+void keep_4_digits_after_point(std::vector<double>& v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        v[i] *= 10000;
+        //printf("Умножили на 10000\n");
+        v[i] = nearbyint(v[i]);
+        //printf("Округляем умноженное число (избавляемся от знаков после точки\n");
+        v[i] /= 10000;
+        //printf("Возвращаем число в исходную форму\n");
+    }
 }
