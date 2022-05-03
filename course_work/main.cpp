ab35adb5ee610e4fceec25b14438b77278572cff
Vlad_Sm <vladislav.smekhnov@gmail.com>
2022-05-03 12:45:03 +0300

    Changed code style in main.cpp

--- course_work/main.cpp
+++ course_work/main.cpp
@@ -6,9 +6,9 @@
 #include "gauss_parallel.h"
 #include "huge_matrix.h"
 
-bool check_answer(std::vector<double> answer, std::vector<double> freeMatrixCloumn, std::vector<std::vector<double>> matrix);
-void create_new_freeColumn(std::vector<double>& v, std::vector<double>& answer, std::vector<std::vector<double>> matrix);
-void keep_4_digits_after_point(std::vector<double>& v);
+void Keep4DigitsAfterPoint(std::vector<double>& v);
+void CreateNewFreeColumn(std::vector<double>& v, std::vector<double>& answer, std::vector<std::vector<double>> matrix);
+bool CheckAnswer(std::vector<double> answer, std::vector<double> freeMatrixCloumn, std::vector<std::vector<double>> matrix);
 
 int main()
 {
@@ -16,12 +16,12 @@ int main()
 
     double timeStart_s, timeEnd_s, tick_s; // время работы последовательной области
     double timeStart_p, timeEnd_p, tick_p; // время работы параллельной области
-    std::vector<double> answer_s; // ответ последовательного решения
-    std::vector<double> answer_p; // ответ параллельного решения
     int matrix_dimension; // размерность матрицы
     double value; // значение элемента массива
+    std::vector<double> answer_s; // ответ последовательного решения
+    std::vector<double> answer_p; // ответ параллельного решения
     std::vector<std::vector<double>> matrix_s, matrix_p, matrix_for_checking; // матрица элементов без столбца свободных членов
-    std::vector<double> freeMatrixColumn_s, freeMatrixColumn_p, freeMatrixColumnForChecking; // столбец свободных членов
+    std::vector<double> freeMatrixColumn_s, freeMatrixColumn_p, free_matrix_column_for_checking; // столбец свободных членов
 
     printf("Решение СЛАУ методом Гаусса\n\n");
     // Задаём размерность матрицы
@@ -30,12 +30,12 @@ int main()
     matrix_s.resize(matrix_dimension);
     matrix_p.resize(matrix_dimension);
     matrix_for_checking.resize(matrix_dimension);
-    printf("\nСоздаю матрицу размером: %d×%d", matrix_dimension, matrix_dimension);
-    create_huge_matrix(matrix_s, 5, matrix_dimension);
-    matrix_for_checking = matrix_p = matrix_s;
 
+    printf("\nСоздаю матрицу размером: %d×%d", matrix_dimension, matrix_dimension);
+    create_huge_matrix(matrix_s, 5, matrix_dimension); // генерируем 1 экземпляр
+    matrix_for_checking = matrix_p = matrix_s; // и все остальные одинаковые
     create_freeMatrixColumn(freeMatrixColumn_s, matrix_dimension);
-    freeMatrixColumnForChecking = freeMatrixColumn_p = freeMatrixColumn_s;
+    free_matrix_column_for_checking = freeMatrixColumn_p = freeMatrixColumn_s;
 
     //// Заполняем матрицу
     //printf("Заполните матрицу (без столбца свободных членов):\n");
@@ -61,12 +61,16 @@ int main()
 
     /* Последовательное решение : */
     printf("\n\nРешаю...\n");
+
     printf("Последовательно...\n");
+
     timeStart_s = omp_get_wtime();
     answer_s = gauss_solving(matrix_s, freeMatrixColumn_s, matrix_dimension);
     timeEnd_s = omp_get_wtime();
     tick_s = omp_get_wtick();
+
     printf("Параллельно...\n");
+
     timeStart_p = omp_get_wtime();
     answer_p = parallel_gauss_solving(matrix_p, freeMatrixColumn_p, matrix_dimension);
     timeEnd_p = omp_get_wtime();
@@ -89,7 +93,7 @@ int main()
     //printf("\n");
     /* Конец параллельного решения */
     
-    // Измеряем время, затраченное на решение СЛАУ обоими методами:
+    // Измеряем время, затраченное на решение СЛАУ обоими способами:
     printf("\nВремя, затраченное в последовательной области:\t%1f\n", timeEnd_s - timeStart_s);
     printf("Время, затраченное в параллельной области:\t%1f\n\n", timeEnd_p - timeStart_p);
     printf("Точность таймера для последовательной области:\t%1f\n", tick_s);
@@ -98,38 +102,37 @@ int main()
     printf("Проверка решения:\n");
     /* Проверка последовательного решения */
     std::cout << std::boolalpha << "Последовательное решение верно:\t" <<
-        check_answer(answer_s, freeMatrixColumnForChecking, matrix_for_checking) << std::endl;
-
+        CheckAnswer(answer_s, free_matrix_column_for_checking, matrix_for_checking) << std::endl;
     /* Проверка параллельного решения */
     std::cout << std::boolalpha << "Параллельное решение верно:\t" <<
-        check_answer(answer_p, freeMatrixColumnForChecking, matrix_for_checking) << std::endl;
+        CheckAnswer(answer_p, free_matrix_column_for_checking, matrix_for_checking) << std::endl;
 
     system("pause");
     return 0;
 }
 
-bool check_answer(std::vector<double> answer, std::vector<double> freeMatrixCloumn, std::vector<std::vector<double>> matrix)
+bool CheckAnswer(std::vector<double> answer, std::vector<double> freeMatrixCloumn, std::vector<std::vector<double>> matrix)
 {
     std::vector<double> myFreeColumn(freeMatrixCloumn.size(), 0.0);
 
-    create_new_freeColumn(myFreeColumn, answer, matrix);
+    CreateNewFreeColumn(myFreeColumn, answer, matrix);
     for (int i = 0; i < freeMatrixCloumn.size(); i++)
         if (myFreeColumn[i] != freeMatrixCloumn[i]) return false;
 
     return true;
 }
 
-void create_new_freeColumn(std::vector<double> &v, std::vector<double>& answer, std::vector<std::vector<double>> matrix)
+void CreateNewFreeColumn(std::vector<double> &v, std::vector<double>& answer, std::vector<std::vector<double>> matrix)
 {
     for (int i = 0; i < matrix.size(); i++)
     {
         for (int j = 0; j < matrix.size(); j++)
             v[i] += matrix[i][j] * answer[j];
     }
-    keep_4_digits_after_point(v);
+    Keep4DigitsAfterPoint(v);
 }
 
-void keep_4_digits_after_point(std::vector<double>& v)
+void Keep4DigitsAfterPoint(std::vector<double>& v)
 {
     for (int i = 0; i < v.size(); i++)
     {
